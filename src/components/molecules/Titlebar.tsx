add85ae67248ea357ce0e581a99eeabd80b834cd
aL0NEW0LF <mohamedfakhreeddine2019@gmail.com>
2023-08-30 22:52:34 +0100

    fix: SideBar breaking at a certain height, dependencies, styling & permissions
    Removed the heavy packages, re-done styling with tailwindcss, refactored permissions, refactored the TitleBar component to be optimal and efficient, fixed the SideBar that was breaking at a certain height (exactly 850px) when the view height is less than that, the Sidebar doesn't take 100% of the window height anymore, so i fixed the min-height at 850px.

--- src/components/molecules/Titlebar.tsx
+++ src/components/molecules/Titlebar.tsx
@@ -1,99 +1,58 @@
-import { createStyles, Menu, Text, UnstyledButton } from '@mantine/core';
-import { useInterval } from '@mantine/hooks';
 import { appWindow } from '@tauri-apps/api/window';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { VscChromeClose, VscChromeMaximize, VscChromeMinimize, VscChromeRestore } from 'react-icons/vsc';
+import { Cross1Icon, MinusIcon, SquareIcon, CopyIcon } from '@radix-ui/react-icons';
+import { useInterval } from 'use-interval';
 
 export function TitleBar() {
   const { t } = useTranslation();
-  const { classes } = getTitleBarStyles();
   const [maximized, setMaximized] = useState(false);
   const [fullscreen, setFullscreen] = useState(false);
-  const [windowTitle, setWindowTitle] = useState('');
 
-  const tauriInterval = useInterval(() => {
+  useInterval(() => {
     appWindow.isMaximized().then(setMaximized);
     appWindow.isFullscreen().then(setFullscreen);
-    appWindow.title().then((title) => {
-      if (windowTitle !== title) setWindowTitle(title);
-    });
   }, 200);
-
-  useEffect(() => {
-    tauriInterval.start();
-    return tauriInterval.stop;
-  }, []);
 
   return (
     <>
       {!fullscreen && (
-        <div data-tauri-drag-region className={classes.titlebar} id="titlebar">
-          <div>
-            <Menu shadow="md" width={200}>
-              <Menu.Target>
-                <UnstyledButton style={{ cursor: 'default' }}></UnstyledButton>
-              </Menu.Target>
-              <Menu.Dropdown>
-                <Menu.Item onClick={() => appWindow.minimize()} icon={<VscChromeMinimize size={14} />}>
-                  {t('Minimize')}
-                </Menu.Item>
-                {maximized ? (
-                  <Menu.Item onClick={() => appWindow.toggleMaximize()} icon={<VscChromeRestore size={14} />}>
-                    {t('Restore Down')}
-                  </Menu.Item>
-                ) : (
-                  <Menu.Item onClick={() => appWindow.toggleMaximize()} icon={<VscChromeMaximize size={14} />}>
-                    {t('Maximize')}
-                  </Menu.Item>
-                )}
-                <Menu.Divider />
-                <Menu.Item
-                  onClick={() => appWindow.hide()}
-                  icon={<VscChromeClose size={14} />}
-                  rightSection={
-                    <Text weight="bold" size="xs">
-                      Alt + F4
-                    </Text>
-                  }
-                >
-                  {t('Close')}
-                </Menu.Item>
-              </Menu.Dropdown>
-            </Menu>
-          </div>
-
+        <div
+          data-tauri-drag-region
+          className="h-8 bg-transparent flex justify-end fixed select-none top-0 w-full z-50 "
+          id="titlebar"
+        >
           <div>
             <div
               title={t('Minimize')}
-              className={`${classes.titlebarButton} hover:bg-foreground active:bg-foreground`}
+              className={`duration-200 inline-flex justify-center items-center h-8 w-12 fill-[#D9D9D9] dark:fill-[#181818] hover:bg-foreground active:bg-foreground`}
               onClick={() => appWindow.minimize()}
             >
-              <VscChromeMinimize className={classes.verticalAlign} />
+              <MinusIcon className="align-middle" />
             </div>
             {maximized ? (
               <div
                 title={t('Restore Down')}
-                className={`${classes.titlebarButton} hover:bg-foreground active:bg-foreground`}
+                className={`uration-200 inline-flex justify-center items-center h-8 w-12 fill-[#D9D9D9] dark:fill-[#181818] hover:bg-foreground active:bg-foreground`}
                 onClick={() => appWindow.toggleMaximize()}
               >
-                <VscChromeRestore className={classes.verticalAlign} />
+                <CopyIcon className="align-middle" />
               </div>
             ) : (
               <div
                 title={t('Maximize')}
-                className={`${classes.titlebarButton} hover:bg-foreground active:bg-foreground`}
+                className={`uration-200 inline-flex justify-center items-center h-8 w-12 fill-[#D9D9D9] dark:fill-[#181818] hover:bg-foreground active:bg-foreground`}
                 onClick={() => appWindow.toggleMaximize()}
               >
-                <VscChromeMaximize className={classes.verticalAlign} />
+                <SquareIcon className="align-middle" />
               </div>
             )}
             <div
               title={t('Close')}
-              className={`${classes.titlebarButton} hover:bg-foreground active:bg-red-500`}
+              className={`uration-200 inline-flex justify-center items-center h-8 w-12 fill-[#D9D9D9] dark:fill-[#181818] hover:bg-foreground active:bg-red-500`}
               onClick={() => appWindow.hide()}
             >
-              <VscChromeClose className={classes.verticalAlign} />
+              <Cross1Icon className="align-middle" />
             </div>
           </div>
         </div>
@@ -101,35 +60,3 @@ export function TitleBar() {
     </>
   );
 }
-
-const getTitleBarStyles = createStyles((theme) => ({
-  verticalAlign: {
-    verticalAlign: 'middle'
-  },
-  titlebar: {
-    height: 30,
-    background: theme.colorScheme === 'dark' ? 'white' : '0 0% 6%',
-    // background: (theme.colorScheme === 'dark') ? theme.colors.dark[7] : 'white',
-    display: 'flex',
-    justifyContent: 'space-between',
-    position: 'fixed',
-    userSelect: 'none',
-    top: 0,
-    left: 0,
-    right: 0,
-    zIndex: 9999,
-    '>div:nth-of-type(2)': {
-      display: 'flex',
-      justifyContent: 'flex-end'
-    }
-  },
-  titlebarButton: {
-    transitionDuration: '200ms',
-    display: 'inline-flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    '>svg': { fill: theme.colorScheme === 'dark' ? 'white' : '0 0% 6%' },
-    width: 47,
-    height: 30
-  }
-}));
--- src/components/molecules/sidebar.tsx
+++ src/components/molecules/sidebar.tsx
@@ -5,7 +5,7 @@ import Menu from './menu';
 const Sidebar = ({ className }: { className?: string }) => {
   return (
     <div
-      className={cn(className, 'flex flex-col bg-foreground')}
+      className={cn(className, 'flex flex-col bg-foreground min-h-[850px]')}
       style={{
         boxShadow: '10.347240447998047px 11.496933937072754px 72.43067932128906px 0px rgba(0, 0, 0, 0.24)'
       }}
